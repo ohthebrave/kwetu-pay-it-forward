53a3fac09431d5da80f1255f44936836bae073f8
George <georgejoj28@gmail.com>
2024-02-10 09:16:00 +0300

    dropdown menu

--- kwetu/src/components/NavBar.jsx
+++ kwetu/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {  NavLink } from "react-router-dom";
-import './nav.css'
+import { NavLink } from "react-router-dom";
+import "./nav.css";
 
 function NavBar() {
   return (
@@ -11,16 +11,29 @@ function NavBar() {
       <nav>
         <ul>
           <li>
-            <NavLink className='bar text-decoration-none fw-light' to='/'>Home</NavLink>
+            <NavLink className="bar text-decoration-none fw-light" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item dropdown">
+                <a className="nav-link drop" href="#">What We Do</a>
+                <div className="dropdown-content">
+                    <NavLink to="/mission">Our Mission</NavLink>
+                    <NavLink to="/vision">Our Vision</NavLink>
+                </div>
+            </li>
+          <li>
+            <NavLink
+              className="bar text-decoration-none fw-light"
+              to="/our-staff"
+            >
+              Our Staff
+            </NavLink>
           </li>
           <li>
-            <NavLink className='bar text-decoration-none fw-light' to='/'>What We Do</NavLink>
-          </li>
-          <li>
-            <NavLink className='bar text-decoration-none fw-light' to='/our-staff'>Our Staff</NavLink>
-          </li>
-          <li>
-            <NavLink className='bar text-decoration-none fw-light' to='/about'>About Us</NavLink>
+            <NavLink className="bar text-decoration-none fw-light" to="/about">
+              About Us
+            </NavLink>
           </li>
         </ul>
       </nav>
